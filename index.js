96d0020b1a79884fce98f04a846c264d3d803eed
Aakash <akash.sharma9268@gmail.com>
2022-05-01 19:16:51 +0530

    private chat messagin completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
+import { saveSession, findSession, findAllSessions } from "./sessionStorage.js";
+import { saveMessage, findMessagesForUser } from "./messageStorage.js";
+
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
@@ -11,14 +14,17 @@ const io = new Server(httpServer, {
 
 //middleware
 io.use((socket, next) => {
-  const sessionId = socket.handshake.sessionId;
-  if(sessionId)
-  {
-    // find my session 
-    const session = null
-    if(session)
-    {
-
+  const sessionId = socket.handshake.auth.sessionId;
+  if (sessionId) {
+    // find my session
+    const session = findSession(sessionId);
+    if (session) {
+      socket.sessionId = sessionId;
+      socket.userId = session.userId;
+      socket.username = session.username;
+      return next();
+    } else {
+      return next(new Error("Invalid Session!"));
     }
   }
 
@@ -30,27 +36,56 @@ io.use((socket, next) => {
   socket.username = username;
   socket.userId = uuidv4();
   socket.sessionId = uuidv4();
-
   next();
 });
+
+function getMessagesForUser(userId) {
+  const messagesPerUser = new Map();
+  findMessagesForUser(userId).forEach((message) => {
+    const { from, to } = message;
+    const otherUser = userId === from ? to : from;
+    if (messagesPerUser.has(otherUser)) {
+      messagesPerUser.get(otherUser).push(message);
+    } else {
+      messagesPerUser.set(otherUser, [message]);
+    }
+  });
+
+  return messagesPerUser;
+}
 
 //connecting to server
 io.on("connection", (socket) => {
   console.log("connection Established", socket.id);
+  saveSession(socket.sessionId, {
+    userId: socket.userId,
+    username: socket.username,
+    connected: true,
+  });
 
+  socket.join(socket.userId);
   //all connected users
   const users = [];
-  for (let [id, socket] of io.of("/").sockets) {
-    users.push({
-      userId: socket.userId,
-      username: socket.username,
-    });
-  }
+  const usermessages = getMessagesForUser(socket.userId);
+  findAllSessions().forEach((session) => {
+    if (session.userId !== socket.userId) {
+      users.push({
+        userId: session.userId,
+        username: session.username,
+        connected: session.connected,
+        messages: usermessages.get(session.userId) || [],
+      });
+    }
+  });
+
   //all users event
   socket.emit("users", users);
-
   //connecting to the users
-  socket.emit("session", { seesionId: socket.sessionId,userId: socket.userId, username: socket.username });
+  socket.emit("session", {
+    sessionId: socket.sessionId,
+    userId: socket.userId,
+    username: socket.username,
+  });
 
   //new user event
   socket.broadcast.emit("user connected", {
@@ -59,12 +94,46 @@ io.on("connection", (socket) => {
   });
 
   //new message event
-  socket.on("new message", (message) => {
-    socket.broadcast.emit("new message", {
-      userId: socket.userId,
-      username: socket.username,
-      message,
+  socket.on("private message", ({ content, to }) => {
+    const newMessage = {
+      from: socket.userId,
+      to,
+      content,
+    };
+    socket.to(to).emit("private message", newMessage);
+    saveMessage(newMessage);
+  });
+
+  socket.on("user messages", ({ userId, username }) => {
+    const userMessages = getMessagesForUser(userId);
+    socket.emit("user message", {
+      userId,
+      username,
+      messages: userMessages.get(socket.userId) || [],
     });
+  });
+
+  socket.on("disconnect", async () => {
+    //returns a promise so we await
+    const matchingSocket = await io.in(socket.userId).allSockets();
+    const isDisconnected = matchingSocket.size === 0;
+    if (isDisconnected) {
+      //notify other users
+      socket.broadcast.emit("user disconnected", {
+        userId: socket.userId,
+        username: socket.username,
+      });
+
+      //update the session
+      saveSession(socket.sessionId, {
+        userId: socket.userId,
+        username: socket.username,
+        connected: socket.connected,
+      });
+    }
+
+    //notify other users
+    //update the session
   });
 });
 
